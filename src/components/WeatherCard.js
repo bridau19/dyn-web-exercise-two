dcdcd1d7d3e9cfcc9300f2889e706ce4280ab496
bridau19 <bridau19@gmail.com>
2021-09-30 10:54:30 -0400

    set up weather api

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -10,19 +10,21 @@ function WeatherCard({
     windSpeed,
 }) {
     return <section className="WeatherCard">
+        <p> Current Temperature </p>
+
+        <p><strong>{ currentTemp }°F</strong></p>
+
         <p>Weather Type: <strong>{ weatherType }</strong></p>
         
-        <p>Low Temp: <strong>{ lowTemp }</strong></p>
+        <p>Low Temperature: <strong>{ lowTemp }</strong>°F</p>
 
-        <p>Current Temp: <strong>{ currentTemp }</strong></p>
+        <p>Cloudiness: <strong>{ cloudiness }%</strong> </p>
 
-        <p>cloudiness: <strong>{ cloudiness }</strong></p>
+        <p>High Temperature: <strong>{ highTemp }</strong>°F</p>
 
-        <p>highTemp: <strong>{ highTemp }</strong></p>
+        <p>Humidity: <strong>{ humidity }%</strong> </p>
 
-        <p>humidity: <strong>{ humidity }</strong></p>
-
-        <p>Wind Speed: <strong>{ windSpeed }</strong></p>
+        <p>Wind Speed: <strong>{ windSpeed } mph</strong></p>
 
     </section>;
 }
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useMemo, useState } from 'react';
+import axios from 'axios';
 import { useLocation } from "react-router-dom";
 import WeatherCard from '../components/WeatherCard';
 
-const API_KEY = '20a967d762faea373696c39bd126aea2';
+const API_KEY = `20a967d762faea373696c39bd126aea2`;
 
 // URL Search Params
 // localhost:3000/?city=paris
@@ -13,30 +14,57 @@ function useQuery() {
 
 function Home() {
     const [city, setCity] = useState();
+    const [weatherData, setWeatherData] = useState();
     let query = useQuery();
+
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=imperial`;
 
     useEffect(() => {
         const cityValue = query.get('city');
         setCity(cityValue);
-    }, []);
+    }, [query]);
+
+    useEffect(() => {
+        // get weather data from weather API
+
+        // allows you to make http requests
+        // promise: do asynchronous requests in JavaScript
+        if (city) {
+            axios
+            .get(URL)
+            .then(function(response) {
+                // successful request, set as weather data
+                setWeatherData(response.data);
+            })
+            .catch(function(error) {
+                // handle error
+                console.warn(error);
+            });
+        }
+    }, [URL, city]); // run every time city changes
 
     const { currentTemp, highTemp, lowTemp, humidity, windSpeed, cloudiness, weatherType } = useMemo(() => {
+        if (!weatherData) return {};
+        console.log(weatherData);
         return ({
-            currentTemp: `75`,
-            highTemp:`88`,
-            lowTemp:`60`,
-            humidity:100,
-            windSpeed:`16 mph`,
-            cloudiness:100,
-            weatherType: "Cloudy"
+            currentTemp: weatherData.main.temp,
+            highTemp: weatherData.main.temp_max,
+            lowTemp: weatherData.main.temp_min,
+            humidity: weatherData.main.humidity,
+            windSpeed: weatherData.wind.speed,
+            cloudiness: weatherData.clouds.all,
+            weatherType: weatherData.weather[0].main,
         })
-    }, []);
+    }, [weatherData]);
 
     return (
         <main className="App">
             <header>
                 <p><a href='/?city=paris'>Paris</a></p>
                 <p><a href='/?city=tokyo'>Tokyo</a></p>
+                <p><a href='/?city=madrid'>Madrid</a></p>
+                <p><a href='/?city=vancouver'>Vancouver</a></p>
+
             </header>
             <h1>{ city }</h1>
             <WeatherCard
